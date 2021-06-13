5e7a1969d9844e78884657c206239fd5b5391d3f
nek0meshi <nekmeshi@gmail.com>
2021-06-13 21:27:14 +0900

    - 簡単なタイマー機能の実装

---
+++ frontend/src/components/Timer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Timer</h1>
+    <p>{{ viewTimerTime }}</p>
+    <button @click="start" :disabled="isRunning">Start</button>
+    <button @click="stop" :disabled="!isRunning">Stop</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import useTimer from '../composable/use-timer'
+
+export default defineComponent({
+  name: 'Timer',
+  props: {
+    msg: {
+      type: String,
+      required: true
+    }
+  },
+  setup: () => {
+    const timer = useTimer()
+
+    return {
+      ...timer,
+    }
+  }
+})
+</script>
